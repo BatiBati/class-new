3b81e5e94bda8f1e7d1c91862073a85cf8b8ea56
Bati <94386708+BatiBati@users.noreply.github.com>
2025-02-04 13:28:31 +0800

    stopWatch writed

--- DOM/index.js
+++ DOM/index.js
@@ -1,24 +1,46 @@
-const buttons = document.querySelectorAll(".container button");
+const [decrease, reset, increase] = document.querySelectorAll(
+  ".buttonContainer button"
+);
+
 const count = document.querySelector(".container p");
 
 let value = 0;
 
-buttons[0].addEventListener("click", () => {
+decrease.addEventListener("click", () => {
   value--;
   count.innerHTML = value;
 });
 
-buttons[1].addEventListener("click", () => {
+reset.addEventListener("click", () => {
   value = 0;
   count.innerHTML = 0;
   count.classList.add("red");
 });
 
-buttons[2].addEventListener("click", () => {
+increase.addEventListener("click", () => {
   value++;
   count.innerHTML = value;
 });
 
-console.log(count);
+const hello = document.querySelectorAll(".text");
 
+console.log(hello);
 
+for (let i = 0; i < hello.length; i++) {
+  hello[i].innerHTML = "Hi";
+}
+
+sum(displayPage, 1, 3);
+
+function sum(callback, x, y) {
+  let result = x + y;
+  callback(result);
+}
+
+function displayConsole(result) {
+  console.log(result);
+}
+
+function displayPage(result) {
+  document.getElementById("myH1").textContent = result;
+}
--- stopWatch/index.js
+++ stopWatch/index.js
@@ -1,38 +1,45 @@
-const realTime = document.querySelector(".realTime");
-const buttons = document.querySelectorAll(".buttonContainer button");
+const [hour, min, sec] = document.querySelectorAll(".timers > div");
+const [startBtn, stopBtn, resetBtn] =
+  document.querySelectorAll(".buttons > button");
 
-let firstValue = 0;
+let duration = 0;
+let intervalID = null;
 
-buttons[0].addEventListener("click", () => {
-  const id = setInterval(() => {
-    firstValue++;
-    realTime.innerHTML = firstValue;
-    console.log(firstValue);
-  }, 500);
+const generateTime = (duration) => {
+  const sec = duration % 60;
+  const min = ((duration - sec) / 60) % 60;
+  const hour = Math.floor(duration / 3600);
+  return {
+    sec,
+    min,
+    hour,
+  };
+};
+
+startBtn.addEventListener("click", () => {
+  if (intervalID !== null) return;
+
+  intervalID = setInterval(() => {
+    duration++;
+    const times = generateTime(duration);
+    sec.innerHTML = times.sec < 10 ? "0" + times.sec : times.sec;
+    min.innerHTML = times.min.toString().padStart(2, "0");
+    hour.innerHTML = times.hour.toString().padStart(2, "0");
+  }, 100);
 });
 
-buttons[2].addEventListener("click", () => {
-  setTimeout(() => {
-    firstValue = 0;
-    realTime.innerHTML = 0;
-  }, timeout);
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalID);
+  intervalID = null;
 });
 
-const value = document.querySelector(".realTime");
+resetBtn.addEventListener("click", () => {
+  clearInterval(intervalID);
+  intervalID = null;
+  duration = 0;
 
-//
-//
-//
-//
-//
-//
-//
-
-// const id = setInterval(() => {
-//   console.log("hello");
-//   console.log(id);
-// }, 1000);
-
-// setTimeout(() => {
-//   clearInterval(id);
-// });
+  const times = generateTime(duration);
+  sec.innerHTML = times.sec < 10 ? "0" + times.sec : times.sec;
+  min.innerHTML = times.min.toString().padStart(2, "0");
+  hour.innerHTML = times.hour.toString().padStart(2, "0");
+});
