84067c708b5836288c61a7abd7a307eec063b3c2
24LP0893 24LP0893 <24LP0893@iMAC2021-05s-iMac.local>
2025-02-18 18:04:28 +0800

    Todo End

--- Framework/ToDo_List/my-todo/src/app/page.js
+++ Framework/ToDo_List/my-todo/src/app/page.js
@@ -5,6 +5,7 @@ import style from "./page.module.css";
 export default function Home() {
   const [value, setValue] = useState("");
   const [list, setList] = useState([]);
+  const [selectedType, setSelectedType] = useState("All");
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -30,47 +31,87 @@ export default function Home() {
     setList(newList);
   };
 
+  const filteredList = list.filter((item, index) => {
+    if (selectedType === "All") return true;
+    if (selectedType === "Completed") return item.isCompleted === true;
+    if (selectedType === "Active") return item.isCompleted == false;
+  });
+
   return (
     <div className={style.container}>
       <h1>To-Do List</h1>
       <div className={style.input}>
-        <input value={value} onChange={handleChange} placeholder="Add a new task..." />
+        <input
+          value={value}
+          onChange={handleChange}
+          placeholder="Add a new task..."
+        />
         <button onClick={handleAdd}>Add</button>
       </div>
 
       <div className={style.types}>
-
-
-
-        <div className={style.toDoTypes} >All</div>
-        <div className={style.toDoTypes}>Active</div>
-        <div className={style.toDoTypes}>Completed</div>
-
-
-
+        <div
+          className={style.toDoTypes}
+          style={{
+            backgroundColor: selectedType === "All" ? "#3c82f6" : "",
+            color: selectedType == "All" ? "white" : "",
+          }}
+          onClick={() => {
+            setSelectedType("All");
+          }}
+        >
+          All
+        </div>
+        <div
+          className={style.toDoTypes}
+          style={{
+            backgroundColor: selectedType === "Active" ? "#3c82f6" : "",
+            color: selectedType == "Active" ? "white" : "",
+          }}
+          onClick={() => {
+            setSelectedType("Active");
+          }}
+        >
+          Active
+        </div>
+        <div
+          className={style.toDoTypes}
+          style={{
+            backgroundColor: selectedType === "Completed" ? "#3c82f6" : "",
+            color: selectedType == "Completed" ? "white" : "",
+          }}
+          onClick={() => {
+            setSelectedType("Completed");
+          }}
+        >
+          Completed
+        </div>
       </div>
 
-      {list.map((item, index) => {
+      {filteredList.map((item, index) => {
         return (
-          <div className={style.todos} key={index}>
+          <div className={style.todos} key={item.value + index}>
             <div className={style.todosItem}>
               <input
                 type="checkbox"
+                checked={item.isCompleted}
                 className={style.cursor}
                 onChange={() => {
                   handleChecked(index);
                 }}
-
               />
-              <p className={item.isCompleted ? style.lineThrough : ""}>{item.value} </p>
+              <p className={item.isCompleted ? style.lineThrough : ""}>
+                {item.value}
+              </p>
             </div>
-            <button className={style.deleteButton} onClick={() => {
-
-
-              handleDelete(index)
-
-
-            }}>Delete</button>
+            <button
+              className={style.deleteButton}
+              onClick={() => {
+                handleDelete(index);
+              }}
+            >
+              Delete
+            </button>
           </div>
         );
       })}
