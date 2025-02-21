5ddfc3675dd9ead3f10b8a5e96a1133a24e7ee58
Bati <94386708+BatiBati@users.noreply.github.com>
2025-02-21 13:34:17 +0800

    testProjectFile

---
+++ Framework/testingFiles/testing_projects/src/app/page.js
@@ -0,0 +1,28 @@
+"use client"
+import { useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const Increment = () => {
+    setCount(count + 1)
+    console.log(count);
+
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <input />
+      <div className={styles.buttonCont}>
+        <div className={styles.decrement} onClick={() => { decrement() }}>Decrement</div>
+        <div className={styles.decrement} onClick={() => { Increment() }}>Increment</div>
+      </div>
+    </div>
+  );
+}
