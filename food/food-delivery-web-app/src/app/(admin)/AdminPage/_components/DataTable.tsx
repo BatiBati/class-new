f18c2e4329002e720831ec77c18913abfd19cbc4
24LP0893 24LP0893 <24LP0893@iMAC2021-05s-iMac.local>
2025-05-20 17:57:06 +0800

    test

--- food/food-delivery-web-app/src/app/(admin)/AdminPage/_components/DataTable.tsx
+++ food/food-delivery-web-app/src/app/(admin)/AdminPage/_components/DataTable.tsx
@@ -111,7 +111,9 @@ export const DataTable = (
             {foodOrder.map((order, index) => (
               <TableRow key={order._id} className="w-full">
                 <TableCell className="p-4 w-[30px]">
-                  <Checkbox checked={checkTarget.includes(order._id)} />
+                  <Checkbox
+                  // checked={checkTarget.includes(order._id)}
+                  />
                 </TableCell>
                 <TableCell className="p-4 w-[30px] align-middle">
                   {index + 1}
